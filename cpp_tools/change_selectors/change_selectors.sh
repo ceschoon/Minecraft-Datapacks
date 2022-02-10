444472c7a415df02f576cb9164391a4bfba1822a
ceschoon <cedric.schoonen@gmail.com>
2022-02-10 22:11:42 +0100

    cpp tools: change selectors

---
+++ cpp_tools/change_selectors/change_selectors.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+dir="."
+x=0
+z_vec=(1000 2000 3000)
+radius=200
+dimension="ccwg:void"
+
+for z in ${z_vec[*]}
+do
+	echo " "
+	echo "Processing z = $z:"
+	
+	ls $dir | grep -i ".mcfunction" | while read entry # handles whitespaces better than the for loop: "for ffile in $(ls $dir | grep -i $author)"
+	do
+		echo "  Converting file: $dir/$entry"
+		./$dir/change_selectors/change_selectors $dimension $x $z $radius $entry
+	done
+	
+	echo " "
+done
+
+
