4b628c84998f135b5c6af1f9fe02b5e7cc38faf9
ceschoon <cedric.schoonen@gmail.com>
2022-02-11 22:43:31 +0100

    cpp tools: removed nbt selection in selectors

--- cpp_tools/change_selectors/change_selectors.cpp
+++ cpp_tools/change_selectors/change_selectors.cpp
@@ -9,40 +9,24 @@
 
 using namespace std;
 
-
-string dim_name_without_colon(string dim_name_in)
-{
-	string dim_name_out;
-	
-	for (int i=0; i<dim_name_in.size(); i++)
-	{
-		if (dim_name_in[i]==':') dim_name_out += "_";
-		else dim_name_out += dim_name_in[i];
-	}
-	
-	return dim_name_out;
-}
-
-
 int main(int argc, char **argv)
 {
 	//////////////////////////////////////////////////////////////
 	
 	if (argc<=3)
 	{
-		cout << "Usage: ./change selectors <dimension> <coord x> <coord z> <radius> <file.mcfunction>" << endl;
+		cout << "Usage: ./change selectors <coord x> <coord z> <radius> <file.mcfunction>" << endl;
 		return 1;
 	}
 	
-	string dimension = argv[1];
-	int coordx = stoi(argv[2]);
-	int coordz = stoi(argv[3]);
-	int radius = stoi(argv[4]);
-	string mcfilename = argv[5];
+	int coordx = stoi(argv[1]);
+	int coordz = stoi(argv[2]);
+	int radius = stoi(argv[3]);
+	string mcfilename = argv[4];
 	
 	//////////////////////////////////////////////////////////////
 	
-	string dir_name = dim_name_without_colon(dimension) + "_" + to_string(coordx) + "_" + to_string(coordz) + "_" + to_string(radius);
+	string dir_name = to_string(coordx) + "_" + to_string(coordz) + "_" + to_string(radius);
 	int sysres = system(("mkdir -p " + dir_name).c_str());
 	
 	stringstream xyz_condition;
@@ -52,9 +36,6 @@ int main(int argc, char **argv)
 	xyz_condition << ",dx=" << 2*radius;
 	xyz_condition << ",dy=" << 2000;
 	xyz_condition << ",dz=" << 2*radius;
-	
-	stringstream nbt_condition;
-	nbt_condition << "nbt={Dimension:\"" << dimension << "\"}";
 	
 	//////////////////////////////////////////////////////////////
 	
@@ -88,8 +69,8 @@ int main(int argc, char **argv)
 			if (line_in[i-1]=='@' && line_in[i]=='a') found_selector = true;
 			if (line_in[i-1]=='@' && line_in[i]=='s') found_selector = true;
 			if (line_in[i-1]=='@' && line_in[i]=='p') found_selector = true;
-			if (line_in[i-1]=='@' && line_in[i]=='e') found_selector = true;
 			if (line_in[i-1]=='@' && line_in[i]=='r') found_selector = true;
+			if (line_in[i-1]=='@' && line_in[i]=='e') found_selector = true;
 			
 			if (found_selector) cout << "Found selector \"@" << line_in[i] << "\" at line " << line_counter << ":" << i << endl;
 			
@@ -97,8 +78,6 @@ int main(int argc, char **argv)
 			{
 				line_out += "[";
 				line_out += xyz_condition.str();
-				line_out += ",";
-				line_out += nbt_condition.str();
 				line_out += "]";
 			}
 			else if (found_selector)
@@ -108,8 +87,6 @@ int main(int argc, char **argv)
 					line_out += "[";
 					line_out += xyz_condition.str();
 					line_out += ",";
-					line_out += nbt_condition.str();
-					line_out += ",";
 					
 					i++; // do not print '[' twice
 				}
@@ -117,8 +94,6 @@ int main(int argc, char **argv)
 				{
 					line_out += "[";
 					line_out += xyz_condition.str();
-					line_out += ",";
-					line_out += nbt_condition.str();
 					line_out += "]";
 				}
 			}
--- cpp_tools/change_selectors/change_selectors.sh
+++ cpp_tools/change_selectors/change_selectors.sh
@@ -2,9 +2,8 @@
 
 dir="."
 x=0
-z_vec=(1000 2000 3000)
+z_vec=(1000 2000 3000 4000 5000 6000 7000 8000)
 radius=200
-dimension="ccwg:void"
 
 for z in ${z_vec[*]}
 do
@@ -14,10 +13,9 @@ do
 	ls $dir | grep -i ".mcfunction" | while read entry # handles whitespaces better than the for loop: "for ffile in $(ls $dir | grep -i $author)"
 	do
 		echo "  Converting file: $dir/$entry"
-		./$dir/change_selectors/change_selectors $dimension $x $z $radius $entry > /dev/null
+		./$dir/change_selectors/change_selectors $x $z $radius $entry > /dev/null
 	done
 	
 	echo " "
 done
-
 
